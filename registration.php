3509cf32ceb5766f849a297317f7e6bc70e7133a
Jisse Reitsma <info@yireo.com>
2017-05-20 14:35:28 +0200

    Pending changes

---
+++ registration.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * SalesBlock2 module for Magento
+ *
+ * @package     Yireo_SalesBlock2
+ * @author      Yireo (https://www.yireo.com/)
+ * @copyright   Copyright 2017 Yireo (https://www.yireo.com/)
+ * @license     Open Source License
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Yireo_SalesBlock2',
+    __DIR__
+);

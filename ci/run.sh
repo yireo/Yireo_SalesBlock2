ae603351c5730ee8665ae1d5d530dbe552879647
Jisse Reitsma <jisse@yireo.com>
2018-08-08 10:56:51 +0200

    Pending changes

--- ci/run.sh
+++ ci/run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-sourcedir=/source
+webdir=/var/www/html
+source=/source
 
-cp /source/magento/integration-tests/install-config-mysql.php $webdir/dev/tests/integration/etc
-cp /source/magento/integration-tests/phpunit.xml $webdir/dev/tests/integration/etc
+cp $source/magento/integration-tests/install-config-mysql.php $webdir/dev/tests/integration/etc
+cp $source/magento/integration-tests/phpunit.xml $webdir/dev/tests/integration
 
 chmod 700 ~/.ssh
 cp ~/ssh/id* ~/.ssh
@@ -15,4 +16,9 @@ chmod 600 ~/.ssh/id*
 composer config repositories.yireo-salesblock2 vcs git@gitlab.yireo.com:magento2-extensions/Yireo_SalesBlock2.git
 composer require yireo/magento2-salesblock2:dev-master
 
+bin/magento module:enable Yireo_SalesBlock2
+bin/magento setup:upgrade
+bin/magento | grep -q salesblock
+
 bin/magento dev:tests:run integration
+

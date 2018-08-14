ad88fe2f058086000ea49ad7b5dde37682a7d933
Jisse Reitsma <jisse@yireo.com>
2018-08-14 09:20:23 +0200

    Cleanup

--- TODO.md
+++
@@ -1,2 +0,0 @@
-# Todo
-- Implement Frontend Label
--- docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-docker run \
-    --rm \
-    --net=magento \
-    --ip=172.20.0.101 \
-    -v ~/.composer/auth.json:/root/.composer/auth.json \
-    -v `pwd`:/module_source \
-    yireo/magento2devbox /module_source/docker/run.sh
-
